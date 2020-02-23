7462a78205c74703dd51fae9dde29f8e5552a644
unknown <dudals789@naver.com>
2020-02-23 12:45:02 +0900

    Updates

---
+++ precache-manifest.fd2fc9d14e3724011819cbd634f52a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5f8660d519fea7c59bacfac8949e821",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "1e54dd084587e0ac0d01",
+    "url": "/movie_app/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "35987439c8be7cfc21ce",
+    "url": "/movie_app/static/js/2.5974a459.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app/static/js/2.5974a459.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e54dd084587e0ac0d01",
+    "url": "/movie_app/static/js/main.7b15f0f1.chunk.js"
+  },
+  {
+    "revision": "ca4b155c3570fabe1dd4",
+    "url": "/movie_app/static/js/runtime-main.98d66c69.js"
+  }
+]);
